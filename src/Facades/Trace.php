e28b5d917bad4ca12eebf10b5bce2c8248942bbe
DizzyLee <xuzhu@changjinglu.net>
2019-11-27 17:38:33 +0800

    fix facade

--- src/Facades/Trace.php
+++ src/Facades/Trace.php
@@ -9,6 +9,14 @@ namespace Meloncut\Trace\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Trace
+ * @package Meloncut\Trace\Facades
+ * @method static start($sign)
+ * @method static next($sign, \Zipkin\Span $span)
+ * @method static finish(\Zipkin\Span $span)
+ * @method static tag(\Zipkin\Span $span, $tag = 'tag', $context = 'content')
+ */
 class Trace extends Facade
 {
 
